46c239d69c4357179d042115da01f53b8f901e60
茸 <84213513+BestLoveForYou@users.noreply.github.com>
2022-12-13 11:31:57 +0800

    Add files via upload

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,1 +1,15 @@
+package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class DemoApplication {
+	public static void main(String[] args){
+		System.out.println("Welcome to use AstralPathTalk!");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
