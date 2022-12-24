c579d26ec962e8425a3377bb4e6337eaa439a50f
茸 <84213513+BestLoveForYou@users.noreply.github.com>
2022-12-24 12:12:18 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,12 +9,8 @@ import com.example.demo.util.TransApi;
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
-	private static final String APP_ID = "20220713001271803";//自己的是啥就输入啥
-	private static final String SECURITY_KEY = "7T7t5VQ2JdjU5JArTtK8";
 	public static void main(String[] args) throws Exception{
 		System.out.println("Welcome to use AstralPathTalk!");
-		//TransApi api = new TransApi(APP_ID, SECURITY_KEY);
-		//String transResult = api.getTransResult("今天下午第1.2节课物理做这张试卷，大家可以提前打印出来。", "auto", "en");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
